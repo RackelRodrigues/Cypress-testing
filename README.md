afb5c9c6529bb9e02d52657acbf0eaa45a9c7667
RackelRodrigues <rackelrodrigues247@gmail.com>
2025-10-18 17:03:10 -0300

    feat: add Cypress tests for web tables, datepickers, sliders, drag & drop, and iframes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cypress Practice Tests
+
+This repository contains practice tests created while learning **Cypress**, a powerful end-to-end testing framework for web applications.  
+It serves as a playground to explore Cypress commands, techniques, and best practices.
+
+## What you'll find here
+
+- Simple tests for basic interactions and assertions.
+- Tests interacting with **UI components** on the Playground site.
+
+## Purpose
+
+This project is dedicated to **learning and experimenting** with Cypress, improving my testing skills, and understanding how to automate complex web interactions efficiently.
--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -126,4 +126,135 @@ it("Dialog Boxes", () => {
     "Are you sure you want to delete?"
   );
 });
-it("Web tables", () => {});
+
+it("Web tables", () => {
+  cy.contains("Tables & Data").check();
+  cy.contains("Smart Table").check();
+
+  //1. How to find by text
+  cy.get("tbody")
+    .contains("tr", "Larry")
+    .then((tableRow) => {
+      cy.wrap(tableRow).find(".nb-edit").click();
+      // the method type() clears the field before typing
+      cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("35");
+      cy.wrap(tableRow).find("nb-checkmark").click();
+      cy.wrap(tableRow).find("td").last().should("have.text", "35");
+    });
+
+  //2. how to find by index
+  cy.get(".nb-plus").click();
+  cy.get("thead tr")
+    .eq(2)
+    .then((tableRow) => {
+      cy.wrap(tableRow).find('[placeholder="First Name"]').type("John");
+      cy.wrap(tableRow).find('[placeholder="Last Name"]').type("Doe");
+      cy.wrap(tableRow).find(".nb-checkmark").click();
+    });
+
+  cy.get("tbody tr")
+    .first()
+    .find("td")
+    .then((tableRows) => {
+      cy.wrap(tableRows).eq(2).should("have.text", "John");
+      cy.wrap(tableRows).eq(3).should("have.text", "Doe");
+    });
+
+  //3. Looping through table rows and columns
+  const ages = [20, 30, 40];
+  cy.wrap(ages).each((age) => {
+    cy.get('[placeholder="Age"]').clear().type(age);
+    // the method wait() is used to pause the test for a specific amount of time, this isn't the good practice
+    cy.wait(500);
+
+    cy.get("tbody tr").each((tableRow) => {
+      if (age === 30) {
+        cy.wrap(tableRow).find("contain.text", "No data found");
+      } else {
+        cy.wrap(tableRow).find("td").last().should("have.text", age);
+      }
+    });
+  });
+});
+
+it("datepickers", () => {
+  cy.contains("Forms").check();
+  cy.contains("Datepicker").check();
+  //in the teste locate the datapicker and select a date
+
+  function selectDateFromCurrentDay(days) {
+    let date = new Date();
+    date.setData(date.getDate() + days);
+    let futureDay = date.getDate();
+
+    // toLocaleString() method returns a string with a language sensitive representation of the date
+    let futureMonthLong = date.toLocaleString("default", { month: "long" });
+    let futureMonthShort = date.toLocaleString("default", { month: "short" });
+
+    // getFullYear() method returns the year of the specified date according to local time
+    let futureYear = date.getFullYear();
+    let dateToAssert = `Aug ${futureMonthShort} ${futureDay}, ${futureYear}`;
+
+    cy.get("nb-calendar-view-mode")
+      .invoke("text")
+      .then((calendarMonthAndYear) => {
+        if (
+          calendarMonthAndYear.includes(futureMonthLong) ||
+          !calendarMonthAndYear.includes(futureYear)
+        ) {
+          // in the if condition we check if the current month and year are different from the future month and year
+          cy.get('[date-name="chevron-right"]').click();
+          selectDateFromCurrentDay();
+        } else {
+          cy.get(".day-cell")
+            .not(".bounding-month")
+            .contains(futureDay)
+            .click();
+        }
+      });
+    return dateToAssert;
+  }
+
+  cy.get('[placeholder="Form Picker"]').then((input) => {
+    cy.wrap(input).click();
+    const dateToAssert = selectDateFromCurrentDay(200);
+    cy.wrap(input).should("have.value", dateToAssert);
+  });
+});
+
+it("Sliders", () => {
+  // In the test, I change the temperature slider value
+
+  cy.get('[tabtitle="Temperature] circle')
+    .invoke("attr", "cy", "38.66")
+    .invoke("attr", "cx", "57,75")
+    .click();
+  cy.get('[class="value temperature').should("contain.text", "18");
+});
+
+it("Drag & Drop", () => {
+  cy.contains("Extra Components").click();
+  cy.contains("Drag & Drop").click();
+
+  // the trigger() with 'dragstart' and 'drop' events is used to simulate drag and drop action
+  //equals to moving an element from one place to another
+  cy.get("#todo-list div").first().trigger("dragstart");
+  cy.get("#drop-list").trigger("drop");
+});
+
+it("iFrames", () => {
+  cy.contains("Modal & Overlays").click();
+  cy.contains("Dialog").click();
+
+  cy.frameLoaded('[data-cy="esc-close-iframe"]');
+  cy.iframe('[data-cy="esc-close-iframe"]')
+    .contains("Open iFrame Dialog")
+    .click();
+
+  cy.contains('[data-cy="esc-close-iframe"]').then((getBody) => {
+    getBody.contains("Open Dialog with esc close").click();
+    cy.contains("Dismiss Dialog").click();
+    getBody.contains("Open Dialog withOut esc close").click();
+    cy.contains("OK").click();
+  });
+});
